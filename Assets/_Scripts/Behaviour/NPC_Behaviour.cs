22f97a489a7e4d1bedc1d15bba957d0f04172796
zakiandiga <rusty_clarinet@yahoo.com>
2021-02-02 23:39:13 -0500

    Update Golem Behaviour
    Simple patrol behaviour

--- Assets/_Scripts/Behaviour/NPC_Behaviour.cs
+++ Assets/_Scripts/Behaviour/NPC_Behaviour.cs
@@ -6,48 +6,178 @@ using UnityEngine.AI;
 public class NPC_Behaviour : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator anim;
 
-    private int actionPoint = 5;
+    [SerializeField] private Transform targetDestinator;
+
+    private int patrolPoint = 5;
 
     private bool isPerforming = false;
+    private bool hasDestination = false;
+    private Vector3 nextDestination;
 
     private GolemState golemState = GolemState.Greetings;
     public enum GolemState
     {
         Greetings,
-        Patrol1,
-        Patrol2,
+        Patrol,        
         Rest,
-        Hyper
+        Hyper,
+        Idle
     }
     
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        //On initiate
+        golemState = GolemState.Greetings;
+        StartCoroutine(GreetingsBehaviour());
 
     }
 
-    private void PerformSwitch()
+    IEnumerator GreetingsBehaviour()
     {
-        golemState = (GolemState)Random.Range(0, 5);
+        Debug.Log("start greeting");
+        float delay = 2f;
+        anim.SetTrigger("greetings");
 
+        yield return new WaitForSeconds(delay);
+
+        Debug.Log("Exit to patrol");
+        golemState = GolemState.Patrol;
+        StartCoroutine(DestinationDelay());
+
+    }
+
+    IEnumerator DestinationDelay()
+    {
+        float targetX = Random.Range(-8, 8);
+        float targetZ = Random.Range(-8, 8);        
+
+        float delay = Random.Range(1.8f, 3f);
+        yield return new WaitForSeconds(delay);
+
+        nextDestination = new Vector3(targetX, targetDestinator.position.y, targetZ);
+        golemState = GolemState.Patrol;
+        
+        SetDestination(nextDestination);
+    }
+
+    private void SetDestination(Vector3 destination)
+    {
+        Debug.Log("SetDestination() executed");
+        hasDestination = true;
+        agent.SetDestination(destination);
+        Debug.Log("Set Destination destination to " + nextDestination);
+        anim.SetBool("isWalking", true);
+
+    }
+
+    IEnumerator DecisionDelay()
+    {        
+        float delay = Random.Range(3, 6);
+        yield return new WaitForSeconds(delay);
+        PerformDecision();
+    }
+
+    private void PerformDecision()
+    {        
+        golemState = (GolemState)Random.Range(1, 3); //doesn't consider Greeting state anymore
+        Debug.Log("exit golemState to " + golemState);
+        isPerforming = true;
+    }
+    
+    private void RefreshPatrolPoints(int points)
+    {
+        patrolPoint += points;
+        PerformDecision();
     }
 
     private void Update()
     {
-        if(!isPerforming)
+        
+
+        if (Vector3.Distance(nextDestination, transform.position) < 1f && golemState == GolemState.Patrol)
         {
-            actionPoint += Random.Range(3, 5);
-            isPerforming = true;
             
-            PerformSwitch();
+            anim.SetBool("isWalking", false);
+            golemState = GolemState.Idle;
+            StartCoroutine(DestinationDelay());
         }
 
-        if(actionPoint < 0)
+        /*
+        switch (golemState)
         {
-            isPerforming = false;
+            case GolemState.Greetings:
+                //do something
+                if(isPerforming)
+                {
+                    Debug.Log("Play greetings animation");
+                    anim.SetTrigger("greetings");
+                    StartCoroutine(DecisionDelay());
+                    isPerforming = false;
+                }                
+                break;
+            case GolemState.Patrol:
+                if(isPerforming)
+                {
+                    if (Vector3.Distance(nextDestination, transform.position) < 0.5f)
+                    {
+                        if (hasDestination)
+                        {
+                            isPerforming = false;
+                            hasDestination = false;
+                        }
+                                                   
+                            
+                    }
+
+                    if (!hasDestination && patrolPoint > 0)
+                    {                        
+                        StartCoroutine(DestinationDelay());
+                        anim.SetBool("isWalking", false);
+                        isPerforming = false;
+                    }
+
+                    if (patrolPoint <= 0)
+                    {
+                        RefreshPatrolPoints(Random.Range(3, 5));                        
+                        StartCoroutine(DecisionDelay());
+                        anim.SetBool("isWalking", false);
+                        isPerforming = false;
+                    }
+                }
+                if(!isPerforming)
+                {
+                    
+                    
+                }
+                //do something
+                break;
+            case GolemState.Hyper:
+                if(isPerforming)
+                {
+                    Debug.Log("Behaviour under development");
+                    StartCoroutine(DecisionDelay());
+                    isPerforming = false;
+                }
+           
+                break;
+            case GolemState.Rest:
+                //Do something
+                if (isPerforming)
+                {
+                    Debug.Log("Behaviour under development");
+                    StartCoroutine(DecisionDelay());
+                    isPerforming = false;
+                }
+                
+                break;
         }
-        
+        */
+
 
 
 
